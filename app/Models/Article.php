69429d9e2b269435c88c6b3f51745f4d591390ad
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-06-19 15:17:43 +0700

    tes

---
+++ app/Models/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $tabel = 'article';
+
+    protected $fillable = ['title', 'content', 'featured_image'];
+}
